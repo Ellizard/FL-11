4ce043e044b1f58154b2d5639b16e198fdc48391
Artem Ostavnenko <artem.ostavnenko@ffwagency.com>
2019-07-14 17:12:06 +0300

    edit code

--- FL11_HW8/homework/js/getMin.js
+++ FL11_HW8/homework/js/getMin.js
@@ -1,10 +1,12 @@
 function getMin () {
   let min = 0;
+
   for (let i=0; i < arguments.length; i ++) {
     if (arguments[i] < min) {
       min = arguments[i];
     }
   }
+
   return min;
 }
 
--- FL11_HW8/homework/js/isInteger.js
+++ FL11_HW8/homework/js/isInteger.js
@@ -1,8 +1,11 @@
 let isInteger = (value) => {
+
   if (isNaN(value)) {
     return false;
   }
+
   let x = parseFloat(value);
+
   return (x | 0) === x;
 };
 
