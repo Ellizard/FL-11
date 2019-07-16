ab59be52b4915e62939d1df406ad6b940af011ef
Artem Ostavnenko <artem.ostavnenko@ffwagency.com>
2019-07-16 17:28:06 +0300

    FL11_HW9

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -1,20 +1,21 @@
 let getNubers = (string) => {
   let numbers = [];
+
   for (let i=0; i < string.length; i++) {
     if (string[i] >=0 || string[i] <= 0) {
       numbers.push(string[i]);
     }
   }
+
   return numbers;
 };
-//console.log(getNubers('n1um3ber95’'));
+
 
 function findTypes() {
   let foundedTypes = {};
 
   for (let i=0; i < arguments.length; i++) {
     let currentType = typeof arguments[i];
-    console.log(currentType);
 
     if (!('' + currentType + '' in foundedTypes) ) {
       foundedTypes['' + currentType + ''] = 0;
@@ -23,9 +24,9 @@ function findTypes() {
       foundedTypes['' + currentType + ''] += 1;
     }
   }
+
   return foundedTypes;
 }
-//console.log(findTypes(null, 5, 'hello'));
 
 
 function executeforEach(arr, f) {
@@ -34,74 +35,67 @@ function executeforEach(arr, f) {
   }
 }
 
-// executeforEach([1,2,3], function(el) {
-//   console.log(el);
-// });
 
-
-// Task 3:
 function mapArray (arr, fun) {
   let nArray = [];
+
   executeforEach(arr, function(x) {
     nArray.push(fun(x));
   });
+
   return nArray;
 }
-// console.log(mapArray([11, 3, 6], function(el) {
-//   return el + 3
-// }));
+
 
 function filterArray (arr, fun) {
   let nArray = [];
+
   executeforEach(arr, function(x){
     if (fun(x)) {
       nArray.push(x);
     }
   });
+
   return nArray;
 }
-// console.log(filterArray([2,5,6], function(el){
-//   return el > 3;
-// }));
 
 
 function showFormattedDate(date) {
   let formattedDate = new Date(date);
   let fullDate;
-
   let month = formattedDate.toLocaleString('en-US', {
     month: 'short'
   });
-
   let day = formattedDate.toLocaleString('en-US', {
     day: 'numeric'
   });
-
   let year = formattedDate.toLocaleString('en-US', {
     year: 'numeric'
   });
 
   fullDate = 'Date: ' + month + ' ' + day + ' ' + year;
-
   return fullDate;
 }
-// console.log(showFormattedDate(new Date('2019-01-27T01:10:00')));
 
 
 function canConvertToDate(date) {
-  return !!Date.parse(date);
+  let dateIsCorrect = !!Date.parse(date);
+  return dateIsCorrect;
 }
-// console.log(canConvertToDate('2016-12-18T00:00:00'));
 
 
 function daysBetween(d1, d2) {
   let date1 = d1.getTime();
   let date2 = d2.getTime();
   let difference = date2 - date1;
-  difference = Math.ceil(difference / (1000 * 60 * 60 * 24));
+  let num = 1000;
+  let min = 60;
+  let hour = 24;
+  difference = Math.ceil(difference / (num * min * min * hour));
+
   return difference;
 }
-//daysBetween(new Date('2016-03-18T00:00:00'), new Date('2016-04-19T00:00:00'));
+
 
 let persons = [
   {
@@ -137,18 +131,24 @@ let persons = [
     'favoriteFruit': 'banana'
   }
 ];
+
+
 function getAmountOfAdultPeople(persons) {
   // Target property.
   let foundProp = ' birthday ';
   // People are over 18
   let personCount = 0;
+  // Days in year.
+  let days = 365;
+  // Adult age
+  let age = 18;
 
   filterArray(persons, function(x) {
       if( x.hasOwnProperty(foundProp) ) {
         let dateNow = new Date();
         let birthTime = new Date(x[foundProp]);
-        let personAge = daysBetween(birthTime, dateNow ) / 365;
-        if (personAge >= 18) {
+        let personAge = daysBetween(birthTime, dateNow ) / days;
+        if (personAge >= age) {
           personCount++;
         }
       }
@@ -156,24 +156,23 @@ function getAmountOfAdultPeople(persons) {
 
   return personCount;
 }
-//console.log(getAmountOfAdultPeople(persons));
 
 
 function keys(o) {
   let array = [];
+
   for (let key in o){
     if (o.hasOwnProperty(key)){
       array.push(key);
     }
   }
+
   return array;
 }
-// console.log(keys({keyOne: 1, keyTwo: 2, keyThree: 3}));
 
 
 function values(o) {
   let array = [];
-
   for (let key in o){
     if (o.hasOwnProperty(key)){
       array.push(o[key]);
@@ -181,7 +180,3 @@ function values(o) {
   }
   return array;
 }
-//console.log(values({keyOne: 1, keyTwo: 2, keyThree: 3}));
-
-
-
